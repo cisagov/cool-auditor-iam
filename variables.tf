d1b21a3e456eb026225614c76a88cfccc96855d1
David Redmin <david.redmin@trio.dhs.gov>
2021-02-05 11:19:36 -0500

    Define required and optional variables

--- variables.tf
+++ variables.tf
@@ -4,9 +4,10 @@
 # You must provide a value for each of these parameters.
 # ------------------------------------------------------------------------------
 
-variable "subnet_id" {
-  type        = string
-  description = "The ID of the AWS subnet to deploy into (e.g. subnet-0123456789abcdef0)"
+variable "auditors" {
+  # Currently-defined audit roles: financial_audit, security_audit
+  description = "A map containing the usernames of each auditor and a list of audit roles assigned to that user.  Example: { \"firstname1.lastname1\" = { \"roles\" = [ \"financial_audit\", \"security_audit\" ] },  \"firstname2.lastname2\" = { \"roles\" = [ \"security_audit\" ] } }"
+  type        = map
 }
 
 # ------------------------------------------------------------------------------
@@ -14,26 +15,74 @@ variable "subnet_id" {
 #
 # These parameters have reasonable defaults.
 # ------------------------------------------------------------------------------
-variable "ami_owner_account_id" {
-  type        = string
-  description = "The ID of the AWS account that owns the Example AMI, or \"self\" if the AMI is owned by the same account as the provisioner."
-  default     = "self"
+
+variable "assume_master_financialaudit_policy_description" {
+  description = "The description to associate with the IAM policy that allows assumption of the role that allows access to the Billing policy in the Master account."
+  default     = "Allow assumption of the FinancialAudit role in the Master account."
 }
 
-variable "aws_availability_zone" {
-  type        = string
-  description = "The AWS availability zone to deploy into (e.g. a, b, c, etc.)"
-  default     = "a"
+variable "assume_master_financialaudit_policy_name" {
+  description = "The name to assign the IAM policy that allows assumption of the role that allows access to the Billing policy in the Master account."
+  default     = "Master-AssumeFinancialAudit"
+}
+
+variable "assume_various_securityaudit_policy_description" {
+  description = "The description to associate with the IAM policy that allows assumption of the role that allows access to the SecurityAudit policy in the various accounts."
+  default     = "Allow assumption of the SecurityAudit role in various accounts."
+}
+
+variable "assume_various_securityaudit_policy_name" {
+  description = "The name to assign the IAM policy that allows assumption of the role that allows access to the SecurityAudit policy in the various accounts."
+  default     = "Various-AssumeSecurityAudit"
 }
 
 variable "aws_region" {
-  type        = string
-  description = "The AWS region to deploy into (e.g. us-east-1)"
+  description = "The AWS region where the non-global resources are to be provisioned (e.g. \"us-east-1\")."
   default     = "us-east-1"
+}
+
+variable "financial_audit_users_group_name" {
+  description = "The name of the group to be created for financial audit users."
+  default     = "financial_auditors"
+}
+
+variable "master_financialaudit_role_description" {
+  description = "The description to associate with the IAM role that allows access to the Billing policy in the Master account."
+  default     = "Allows sufficient access to billing information."
+}
+
+variable "master_financialaudit_role_name" {
+  description = "The name to assign the IAM role that allows access to the Billing policy in the Master account."
+  default     = "FinancialAudit"
+}
+
+variable "security_audit_users_group_name" {
+  description = "The name of the group to be created for security audit users."
+  default     = "security_auditors"
+}
+
+variable "securityauditextras_policy_description" {
+  description = "The description to associate with the IAM policy that gives access to additional permissions required by security auditors."
+  default     = "Allows access to additional resources required by security auditors."
+}
+
+variable "securityauditextras_policy_name" {
+  description = "The name to assign the IAM policy that gives access to additional permissions required by security auditors."
+  default     = "SecurityAuditExtras"
 }
 
 variable "tags" {
   type        = map(string)
   description = "Tags to apply to all AWS resources created"
   default     = {}
+}
+
+variable "various_securityaudit_role_description" {
+  description = "The description to associate with the IAM role that allows access to the SecurityAudit policy in various AWS accounts."
+  default     = "Allows read-only access to resources for security auditors."
+}
+
+variable "various_securityaudit_role_name" {
+  description = "The name to assign the IAM role that allows access to the SecurityAudit policy in various AWS accounts."
+  default     = "SecurityAudit"
 }
