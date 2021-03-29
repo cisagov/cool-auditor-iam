940b97ea34c31699111f253a504053ffb62ad09c
David Redmin <david.redmin@trio.dhs.gov>
2021-03-29 13:44:02 -0400

    Use cleaner Terraform syntax to generate user sets

--- group_membership.tf
+++ group_membership.tf
@@ -2,7 +2,7 @@
 resource "aws_iam_user_group_membership" "financial_audit" {
   provider = aws.users
 
-  for_each = { for username, attributes in var.auditors : username => "" if contains(attributes["roles"], "financial_audit") }
+  for_each = toset([for username, attributes in var.auditors : username if contains(attributes["roles"], "financial_audit")])
 
   user = data.aws_iam_user.auditors[each.key].user_name
 
@@ -15,7 +15,7 @@ resource "aws_iam_user_group_membership" "financial_audit" {
 resource "aws_iam_user_group_membership" "security_audit" {
   provider = aws.users
 
-  for_each = { for username, attributes in var.auditors : username => "" if contains(attributes["roles"], "security_audit") }
+  for_each = toset([for username, attributes in var.auditors : username if contains(attributes["roles"], "security_audit")])
 
   user = data.aws_iam_user.auditors[each.key].user_name
 
