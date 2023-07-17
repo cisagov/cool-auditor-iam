47ea48c12315000a600c794e8067060d7818cfcc
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2023-07-17 13:16:04 -0400

    Attach standard AWS purchase order policy to financial audit role

--- master_financialaudit_role.tf
+++ master_financialaudit_role.tf
@@ -33,3 +33,11 @@ resource "aws_iam_role_policy_attachment" "billing_policy_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/job-function/Billing"
   role       = aws_iam_role.master_financialaudit_role.name
 }
+
+# Attach (standard AWS-defined) Purchase Order management policy to the role
+resource "aws_iam_role_policy_attachment" "purchaseorder_policy_attachment" {
+  provider = aws.master
+
+  policy_arn = "arn:aws:iam::aws:policy/AWSPurchaseOrdersServiceRolePolicy"
+  role       = aws_iam_role.master_financialaudit_role.name
+}
